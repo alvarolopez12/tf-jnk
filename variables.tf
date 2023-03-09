ac36cd8a45ed6f268bd6c70bcfa5f0e75e0cd08f
Álvaro López Álvarez <alvaro.lopezalv@hotmail.com>
2023-03-09 10:54:27 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -9,3 +9,30 @@ variable "resource_group_name_prefix" {
   default     = "rg"
   description = "Prefix of the resource group name that's combined with a random ID so name is unique in your Azure subscription."
 }
+
+
+#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#
+#*                 Root Module                         *#
+#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#
+
+# Service Principal Variables
+
+variable "client_id" {
+    description =   "Client ID (APP ID) of the application"
+    type        =   string
+}
+
+variable "client_secret" {
+    description =   "Client Secret (Password) of the application"
+    type        =   string
+}
+
+variable "subscription_id" {
+    description =   "Subscription ID"
+    type        =   string
+}
+
+variable "tenant_id" {
+    description =   "Tenant ID"
+    type        =   string
+}
